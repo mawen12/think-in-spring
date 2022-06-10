9aa91a5de3f9b25987e76453045e2a5090f2d8c4
mawen12 <1181963012mw@gmail.com>
2022-06-10 08:13:52 +0800

    API 实现 After Advice

--- aop-features/src/main/java/com/mawen/think/in/spring/aop/features/AspectJAnnotationUsingAPIDemo.java
+++ aop-features/src/main/java/com/mawen/think/in/spring/aop/features/AspectJAnnotationUsingAPIDemo.java
@@ -1,7 +1,10 @@
 package com.mawen.think.in.spring.aop.features;
 
 import com.mawen.think.in.spring.aop.features.aspect.AspectConfiguration;
+import org.springframework.aop.AfterAdvice;
+import org.springframework.aop.AfterReturningAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.aop.aspectj.AspectJAfterAdvice;
 import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
 
 import java.lang.reflect.Method;
@@ -28,7 +31,21 @@ public class AspectJAnnotationUsingAPIDemo {
             @Override
             public void before(Method method, Object[] args, Object target) throws Throwable {
                 if ("put".equals(method.getName()) && args.length == 2) {
-                    System.out.printf("当前存放的是 Key : %s, Value : %s \n", args[0], args[1]);
+                    System.out.printf("[MethodBeforeAdvice] 当前存放的是 Key : %s, Value : %s \n", args[0], args[1]);
+                }
+            }
+        });
+
+        // 添加 AfterReturningAdvice
+        proxyFactory.addAdvice(new AfterReturningAdvice() {
+            @Override
+            public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+                if ("put".equals(method.getName()) && args.length == 2) {
+                    System.out.printf("[AfterReturningAdvice] 当前存放的是 Key : %s, 新存放的 Value : %s, 之前关联的 Value : %s \n",
+                        args[0],    // key
+                        args[1],    // new value
+                        returnValue // old value
+                    );
                 }
             }
         });
@@ -38,6 +55,7 @@ public class AspectJAnnotationUsingAPIDemo {
         // 通过代理对象存储
         Map<String, Object> proxy = proxyFactory.getProxy();
         proxy.put("1", "A");
+        proxy.put("1", "B");
         System.out.println(proxy.get("1"));
     }
 
